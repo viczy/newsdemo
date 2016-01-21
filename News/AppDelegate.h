e2b4fdb5abc501eb9a2b08b579e6124fca7cf1f5
vic <vic@mbp.local>
2016-01-21 11:35:47 +0800

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by vic on 16/1/21.
+//  Copyright © 2016年 NW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
